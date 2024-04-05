bf61f702b8d0dc91deb045dcd3c1ad0ed3380b41
HARSH KUMAR GUPTA <133455584+hars728@users.noreply.github.com>
2024-04-05 19:52:33 +0530

    first react project

--- harsh-app/src/App.js
+++ harsh-app/src/App.js
@@ -1,15 +1,85 @@
+import ExpenseItem from './Components/ExpenseItem';
+
 import './App.css'
 
-function App (){
 
-  return(
+function App() {
+  let expenses = [
+    {
+      id: 'el',
+      title: 'school - Fee',
+      amount: 250,
+      date: new Date(2021, 5, 12)
+    },
+    {
+      id: 'e1',
+      title: 'school - Fee',
+      amount: 250,
+      date: new Date(2021, 5, 12)
+    },
+    {
+      id: 'e2',
+      title: 'other  - Fee',
+      amount: 250,
+      date: new Date(2021, 6, 2)
+    },
+    {
+      id: 'e3',
+      title: 'house rent',
+      amount: 250,
+      date: new Date(2021, 8, 10)
+    },
+    {
+      id: 'e4',
+      title: 'new - Fee',
+      amount: 20,
+      date: new Date(2025, 5, 12)
+    },
+    {
+      id: 'e5',
+      title: 'domestic- Fee',
+      amount: 2000,
+      date: new Date(2025, 9, 12)
+    }
+  ];
 
-  <div className="container">
-    <h2>this is app side</h2>
-    <h2>In this app container we will connect the component</h2>
-    
-  </div>
+  return (
+
+    <div className="container">
+      <h2>Let's get started</h2>
+      <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        amount={expenses[0].amount}>
+      </ExpenseItem>
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        amount={expenses[1].amount}>
+      </ExpenseItem>
+      <ExpenseItem
+        date={expenses[2].date}
+        title={expenses[2].title}
+        amount={expenses[2].amount}>
+      </ExpenseItem>
+      <ExpenseItem
+        date={expenses[3].date}
+        title={expenses[3].title}
+        amount={expenses[3].amount}>
+      </ExpenseItem>
+      <ExpenseItem
+        date={expenses[4].date}
+        title={expenses[4].title}
+        amount={expenses[4].amount}>
+      </ExpenseItem>
+      <ExpenseItem
+        date={expenses[5].date}
+        title={expenses[5].title}
+        amount={expenses[5].amount}>
+      </ExpenseItem>
+
+    </div>
   );
-}
 
-export default App;
+}
+  export default App;
