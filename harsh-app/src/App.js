23264e26090c8cf5cc9d34206535f3dd2cd52c94
HARSH KUMAR GUPTA <133455584+hars728@users.noreply.github.com>
2024-04-05 15:02:00 +0530

    first commit

---
+++ harsh-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App (){
+
+  return(
+
+  <div className="container">
+    <h2>this is app side</h2>
+    <h2>In this app container we will connect the component</h2>
+    
+  </div>
+  );
+}
+
+export default App;
