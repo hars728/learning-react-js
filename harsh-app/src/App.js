42273636042aaf068f8cdae780686e38fccd855d
HARSH KUMAR GUPTA <133455584+hars728@users.noreply.github.com>
2024-04-06 13:40:01 +0530

    descuss about the multiple component & nested component

--- harsh-app/src/App.js
+++ harsh-app/src/App.js
@@ -1,6 +1,8 @@
-import ExpenseItem from './Components/ExpenseItem';
 
+import React from 'react';
 import './App.css'
+
+import Expenses from './Components/EXPENSES/Expenses';
 
 
 function App() {
@@ -47,37 +49,7 @@ function App() {
 
     <div className="container">
       <h2>Let's get started</h2>
-      <ExpenseItem
-        date={expenses[0].date}
-        title={expenses[0].title}
-        amount={expenses[0].amount}>
-      </ExpenseItem>
-      <ExpenseItem
-        date={expenses[1].date}
-        title={expenses[1].title}
-        amount={expenses[1].amount}>
-      </ExpenseItem>
-      <ExpenseItem
-        date={expenses[2].date}
-        title={expenses[2].title}
-        amount={expenses[2].amount}>
-      </ExpenseItem>
-      <ExpenseItem
-        date={expenses[3].date}
-        title={expenses[3].title}
-        amount={expenses[3].amount}>
-      </ExpenseItem>
-      <ExpenseItem
-        date={expenses[4].date}
-        title={expenses[4].title}
-        amount={expenses[4].amount}>
-      </ExpenseItem>
-      <ExpenseItem
-        date={expenses[5].date}
-        title={expenses[5].title}
-        amount={expenses[5].amount}>
-      </ExpenseItem>
-
+      <Expenses item= {expenses} />
     </div>
   );
 
