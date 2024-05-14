7fbe8731bd9ee82846c755994a4ddb9e7ab48b7f
HARSH KUMAR GUPTA <133455584+hars728@users.noreply.github.com>
2024-05-14 04:10:12 +0000

    chenging hooks

--- 01_hooks/src/App.jsx
+++ 01_hooks/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import './App.css'
 
 
 function App() {
@@ -8,11 +9,24 @@ function App() {
     let [ counter ,setCounter]=useState(7)
    const addValue=()=>{
     counter= counter+1
-    setCounter(counter)
+    if (counter>=21) {
+      alert("limit exceded")
+      
+    }
+    else{
+      setCounter(counter)
+    }
+    
    }
    const removeValue =()=>{
     counter=counter-1
-    setCounter(counter)
+    if (counter<=-1){
+      alert("negativ valure no allowed")
+    }
+    else{
+      setCounter(counter)
+    }
+    
    }
   return (
     <>
