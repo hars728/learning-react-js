88b3dcc02a0d8a6f976074b2b606d59e5de795e6
HARSH KUMAR GUPTA <133455584+hars728@users.noreply.github.com>
2024-07-09 17:22:43 +0530

    hooks

--- 01_hooks/src/App.jsx
+++ 01_hooks/src/App.jsx
@@ -5,37 +5,37 @@ import './App.css'
 
 
 function App() {
-  
-    let [ counter ,setCounter]=useState(7)
-   const addValue=()=>{
-    counter= counter+1
-    if (counter>=21) {
-      alert("limit exceded")
-      
+
+  let [counter, setCounter] = useState(7)
+  const addValue = () => {
+    counter = counter + 1
+    if (counter >= 21) {
+      alert("limit exceded ")
+
     }
-    else{
+    else {
       setCounter(counter)
     }
-    
-   }
-   const removeValue =()=>{
-    counter=counter-1
-    if (counter<=-1){
+
+  }
+  const removeValue = () => {
+    counter = counter - 1
+    if (counter <= -1) {
       alert("negativ valure no allowed")
     }
-    else{
+    else {
       setCounter(counter)
     }
-    
-   }
+
+  }
   return (
     <>
       <h1>hello harsh</h1>
       <h2>counter value {counter} </h2>
 
       <button
-       onClick={addValue}
-       >Add value
+        onClick={addValue}
+      >Add value
 
       </button>
       <button onClick={removeValue}>
